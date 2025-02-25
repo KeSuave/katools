af6f95a79afbfd369286de016acaa71b06368bd4
KeSuave <kesuave79@gmail.com>
2025-02-24 22:28:58 -0800

    feat: components on entity

--- src/templates/entity.ts
+++ src/templates/entity.ts
@@ -1,8 +1,13 @@
-export default `import type { GameObj } from "kaplay";
+export default `import type { GameObj, PosComp, Vec2 } from "kaplay";
 
 import { k } from "../context";
 
-export function make<%pascalCaseName%>(): GameObj {
-  return k.make(["<%name%>"]);
+export type <%pascalCaseName%>Comps = PosComp;
+
+export function make<%pascalCaseName%>(pos: Vec2): GameObj<<%pascalCaseName%>Comps> {
+  return k.make([
+    k.pos(pos),
+    "<%name%>"
+  ]);
 }
 `
